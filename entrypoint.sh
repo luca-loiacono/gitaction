6cb09ae92901e9f1759458711b4c8202b45c1d09
Luca Lo Iacono <luca.loiacono@dedalus.eu>
2022-05-11 23:54:52 +0200

    add steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
-MYIP=$(curl ifconfig.me)
+myip=$(curl ifconfig.me)
 
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
-echo "I wrote from this IP: $MYIP"
+echo "I wrote from this IP: $myip"
